55e41cf6e9c3a488903a1e976faef93925f99d87
dmitruz <dmitruz970@gmail.com>
2022-01-22 13:34:37 +0200

    finished

---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { createPortal } from "react-dom";
+import { Overlay, ModalContainer } from "./Modal.styled";
+
+const modalRoot = document.querySelector("#modal-root");
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleKeyDown = (e) => {
+    if (e.code === "Escape") {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = (e) => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <Overlay onClick={this.handleBackdropClick}>
+        <ModalContainer>{this.props.children}</ModalContainer>
+      </Overlay>,
+      modalRoot
+    );
+  }
+}
+
+export default Modal;
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
--- src/Helpers/ImagesMapper.js
+++
@@ -1,9 +0,0 @@
-const ImagesMapper = arr =>
-  arr.map(el => ({
-    id: el.id,
-    webformatURL: el.webformatURL,
-    largeImageURL: el.largeImageURL,
-    tags: el.tags,
-  }));
-
-export default ImagesMapper;
